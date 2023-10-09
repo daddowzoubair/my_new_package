b950d1c23decdcdbfe39a6430b11804ed7d0ca5b
daddowzoubair <daddow.zoubair@gmail.com>
2023-10-09 15:54:39 +0400

    Add test functions

--- lib/my_sports_model.dart
+++ lib/my_sports_model.dart
@@ -2,6 +2,10 @@ library my_sports_model;
 
 /// A Calculator.
 class Calculator {
+  int _counter = 0;
   /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  /// 
+  int get counter => _counter; 
+
+  int addOne() => ++_counter;
 }
--- test/my_sports_model_test.dart
+++ test/my_sports_model_test.dart
@@ -5,8 +5,9 @@ import 'package:my_sports_model/my_sports_model.dart';
 void main() {
   test('adds one to input values', () {
     final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    expect(calculator.addOne(), 1);
+    expect(calculator.addOne(), 2);
+    expect(calculator.addOne(), 3);
+    expect(calculator.addOne(), 4);
   });
 }
